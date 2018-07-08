ae87590cf5e8a6e91029e3803dcf6059a36b63c7
ayushpandey56139 <ayushpandey5613@gmail.com>
2018-07-08 07:34:15 +0530

    this is 4th part of object oriented programming in python

---
+++ oop4.py
@@ -0,0 +1,20 @@
+class shark:
+    def swim(self):
+       print("the shark is swiming ")
+    def swim_backwoeds(self):
+       print("the shark can not swim backword ")
+    def skeleton(self):
+       print("the shark skeleton is made of wood ")
+
+class clownfish():
+    def swim(self):
+      print("the clownfish is swiming ")
+    def swim_backword(self):
+      print("the clown fish is swimimng ")
+    def skeleton(self):
+      print("the clownfish skeleton is made of iron ")
+
+s=shark()
+s.swim()
+c=clownfish()
+c.swim()
